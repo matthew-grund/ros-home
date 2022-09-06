f7bb4b9cf714893302396a69277d9f074533e87c
Matthew Grund <matthew.grund77@gmail.com>
2022-09-06 16:01:13 -0400

    weather track uses settings topic to get location information

--- src/home_core/home_core/weather_track.py
+++ src/home_core/home_core/weather_track.py
@@ -18,16 +18,21 @@ class WeatherTracker(Node):
         self.publisher_alerts = self.create_publisher(String, 'wx_alerts', 10)      
         self.timer_period = 30.0  # seconds
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        self.config_subscription = self.create_subscription(
+            String,
+            'settings',
+            self.config_listener_callback,
+            10)
+        self.config_subscription
         self.i = 0
-        self.city = "Mashpee"             # FIXME: add to an INI file, or parameter server 
-        self.state = "Massachusetts"
-        self.tzname = "America/New_York"
-        self.latitude = 41.619
-        self.longitude = -70.451
         self.need_forecast_url = True
         self.need_obs_station_url = True
-
+        self.need_config_location = True
+        
     def timer_callback(self):
+        if self.need_config_location:
+            return  # don't do anything until we get the home location
+
         if self.need_forecast_url:
             '''https://api.weather.gov/points/41.619,-70.451'''
             get_url =  "https://api.weather.gov/points/%.4f,%.4f" % (self.latitude,self.longitude)
@@ -71,7 +76,7 @@ class WeatherTracker(Node):
                 msg.data = json.dumps(m)
                 self.publisher_conditions.publish(msg)
                 if (type(observations['temperature']['value']) == int) or \
-                    (type(observations['temperature']['value']) == int):
+                    (type(observations['temperature']['value']) == float):
                     self.get_logger().info('Current: "%s" %.1f F' % (observations['textDescription'],observations['temperature']['value']/5*9+32))
                 else:
                     self.get_logger().info('Current: "%s" (NULL temp)' % observations['textDescription'])
@@ -96,6 +101,18 @@ class WeatherTracker(Node):
                 self.get_logger().info('Forecast: "%s"' % forecast[1]['detailedForecast'])
                 self.get_logger().info('Forecast: "%s"' % forecast[2]['detailedForecast'])
                 self.i += 1
+
+    def config_listener_callback(self,msg):
+        msg = json.loads(msg.data)
+        if msg['payload']['type'] == "LOCATION":
+            self.latitude = float(msg['payload']['Coordinates']['latitude']) 
+            self.longitude = float(msg['payload']['Coordinates']['longitude']) 
+            self.city = msg['payload']['City']['city']              
+            self.state = msg['payload']['City']['state']
+            self.tzname = msg['payload']['Timezone']['tzname']
+            self.need_config_location = False
+            self.get_logger().info(f"Got config: Location is {self.city}, {self.state}")
+
 
 def main(args=None):
     rclpy.init(args=args)
