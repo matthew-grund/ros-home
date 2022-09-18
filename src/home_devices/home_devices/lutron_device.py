ad13b73eaef3cfe3b9b588d92cd6b721799b9351
Matthew Grund <matthew.grund77@gmail.com>
2022-09-18 15:02:39 -0400

    adding lutron device handler

--- src/home_devices/home_devices/lutron_device.py
+++ src/home_devices/home_devices/lutron_device.py
@@ -9,18 +9,18 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-class HomeScheduler(Node):
+class LutronDevice(Node):
 
     def __init__(self):
-        super().__init__('home_scheduler')
-        self.publisher_ = self.create_publisher(String, 'appointments', 10)
-        timer_period = 0.5  # seconds
+        super().__init__('lutron_device')
+        self.publisher_ = self.create_publisher(String, 'lutron_device_status', 10)
+        timer_period = 2.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Appointment: %d' % self.i
+        msg.data = 'Light Status: %d' % self.i
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
@@ -29,11 +29,11 @@ class HomeScheduler(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    home_scheduler = HomeScheduler()
+    device = LutronDevice()
 
-    rclpy.spin(home_scheduler)
+    rclpy.spin(device)
 
-    home_scheduler.destroy_node()
+    device.destroy_node()
     rclpy.shutdown()
 
 
