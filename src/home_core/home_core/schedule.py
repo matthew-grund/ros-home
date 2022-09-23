d262eeb6f8a5e15f3f89aec87d492e24b7c311d3
Matthew Grund <matthew.grund77@gmail.com>
2022-09-23 15:45:26 -0400

    scheduler works now.

--- src/home_core/home_core/schedule.py
+++ src/home_core/home_core/schedule.py
@@ -7,9 +7,9 @@ from cmath import nan
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import datetime
-import time
+from datetime import date, time, datetime, timedelta
 import json
+from dateutil import parser
 
 
 class HomeScheduler(Node):
@@ -17,8 +17,8 @@ class HomeScheduler(Node):
     def __init__(self):
         super().__init__('home_scheduler')
         self.publisher_ = self.create_publisher(String, 'scene', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.timer_period = 3.0  # seconds
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
         self.config_subscription = self.create_subscription(
             String,
             'settings',
@@ -34,78 +34,106 @@ class HomeScheduler(Node):
         self.i = 0
         self.need_config_schedule = True
         self.need_named_events = True
+        self.schedule = {}
+        self.raw_schedule = {}
 
     def timer_callback(self):
         msg = String()
         m={}
-        m['today'] = self.today
-        msg.data = 'Scene: %d' % self.i
+        m['index'] = self.i
+        m['interval'] = self.timer_period
+        m['payload'] = {}
+        m['payload']['scenes'] = self.schedule
+        m['payload']['today'] = self.today_str()
+        msg.data = json.dumps(m)
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing schedule with %d scenes' % len(m['payload']['scenes']))
+        self.i += 1
+        
 
     def config_listener_callback(self,msg):
-        if self.need_sun_events == True:
-            return
         msg = json.loads(msg.data)
         if msg['payload']['type'] == "SCHEDULE":
-            raw_schedule = msg['payload']
-            self.schedule = self.process_raw_schedule(raw_schedule)
+            self.raw_schedule = msg['payload']
+            self.get_logger().info(f"Got scehdule: %s" % json.dumps(self.raw_schedule))
+            self.schedule = self.process_raw_schedule(self.raw_schedule)
             self.need_config_schedule = False
-            self.get_logger().info(f"Got scehdule: %s" % json.dumps(self.schedule))
-
+            
     def process_raw_schedule(self, raw):
         schedule = {}
         # expand raw to the scene schedule
         for scene in raw:
             if (scene != 'type') and (scene!= 'filepath'):
                 # make the day array
-                if raw[scene].has_key('day'):
-                    raw[scene]['day'] = self.expand_day_str(raw[scene]['day'])
+                if 'day' in raw[scene]:
+                    raw[scene]['day'] = self.parse_day_str(raw[scene]['day'])
+                    self.get_logger().info(f"Scene '%s' has 'day' '%s'" % (scene,raw[scene]['day']))
                 else:
                     raw[scene]['day'] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-                if raw[scene].has_key('time'):
-                   raw[scene]['time_sec'] = self.convert_time_str(raw[scene]['time'])
+                    self.get_logger().warn(f"Scene '%s' has no entry 'day' (will run every day)" % scene)
+                # compute the seconds since midnight    
+                if 'time' in raw[scene]: 
+                   raw[scene]['time_seconds'] = self.parse_time_str(raw[scene]['time'])
+                   self.get_logger().info(f"Scene '%s' has 'time' '%.1f'" % (scene,raw[scene]['time_seconds']))
+                else:
+                   raw[scene]['time_seconds'] = nan  
+                    
+                if raw[scene]['time_seconds'] != nan: 
+                    print(f"adding scene '%s' to schedule" % scene )      
+                    schedule[scene] = raw[scene]
+                else:
+                    self.get_logger().error(f"Scene '%s' time not parsed" % scene)
+        if len(schedule): 
+            # self.show_schedule(schedule)   
+            self.get_logger().info(f"Scene schedule: %s" % json.dumps(schedule))
+        else:
+            self.get_logger().warn(f"No scene schedule, yet.")    
+        return schedule
 
-
-    def expand_day_str(self,day_cfg_str):
+    def parse_day_str(self,day_cfg_str_or_list):
         day_list = []
-        for day_str in day_cfg_str:
-            if day_str.title()=='All' or day_str.title()=='Everyday':
-                day_list.extend(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
-            elif day_str.title()=='Weekdays' or day_str.title()=='Workdays':
-                day_list.extend(["Monday", "Tuesday", "Wednesday", "Thursday","Friday"])
-            elif day_str.title()=='Weekend'or day_str.title()=='Weekends':
-                day_list.extend(["Saturday", "Sunday"])
-            else:
-                day_list.extend(day_str)    
+        if isinstance(day_cfg_str_or_list,list):  # is it a list or just a value (string?)
+            for day_str in day_cfg_str_or_list:
+                    day_list = self.append_special_string(day_list, day_str)
+        else:
+            day_list = self.append_special_string(day_list,day_cfg_str_or_list)
         return day_list
 
-    def today_str(self):
-        now = datetime.datetime.now()
-        self.today = now.strftime("%A")
-        return self.today
-
-    def sun_listener_callback(self,msg):
-        msg = json.loads(msg.data)
-        self.named_events = msg['payload']['events']
-        self.need_named_events = False        
-
-    def convert_time_str(self,timestr):
+    def append_special_string(self,in_list,day_str):
+        day_list = in_list
+        if day_str.title()=='All' or day_str.title()=='Everyday':
+            day_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        elif day_str.title()=='Weekdays' or day_str.title()=='Workdays':
+            day_list.append("Monday")
+            day_list.append("Tuesday")
+            day_list.append("Wednesday")
+            day_list.append("Thursday")
+            day_list.append("Friday")
+        elif day_str.title()=='Weekend'or day_str.title()=='Weekends':
+            day_list.append("Saturday")
+            day_list.append("Sunday")
+        else:
+            day_list.append(day_str)    
+        return day_list    
+   
+    def parse_time_str(self,timestr):
         pluses = timestr.count('+')
         minuses = timestr.count('-')
         if minuses + pluses > 1:
-            self.get_logger().error(f"Can't parse time: %s (expression too complex)" % timestr)
+            self.get_logger().error(f"Can't parse time: '%s' (expression too complex)" % timestr)
         if pluses > 1:
-            self.get_logger().error(f"Can't parse time: %s (too many pluses)" % timestr)
+            self.get_logger().error(f"Can't parse time: '%s' (too many pluses)" % timestr)
+        if minuses > 1:
+            self.get_logger().error(f"Can't parse time: '%s' (too many minuses)" % timestr)
         if pluses == 1:
             split = timestr.split('+',2)
-            namedevent = split[0]
-            tstr = split[1]
+            namedevent = split[0].strip()
+            tstr = split[1].strip()
             mul = 1.0
         elif minuses == 1:
             split = timestr.split('-',2)
-            namedevent = split[0]
-            tstr = split[1]
+            namedevent = split[0].strip()
+            tstr = split[1].strip()
             mul = -1.0
         else:
             namedevent=''
@@ -113,17 +141,55 @@ class HomeScheduler(Node):
             mul = 1.0
         colons = tstr.count(':')
         if colons == 2:
-            st = time.strptime(tstr,'%H:%M:%S')
-            seconds = datetime.timedelta(hours=st.tm_hour,minutes=st.tm_min,seconds=st.tm_sec).total_seconds()
+            dt = parser.parse(tstr)
+            seconds = timedelta(hours=dt.hour,minutes=dt.minute,seconds=dt.second).total_seconds()
         elif colons == 1:
-            st = time.strptime(tstr,'%H:%M')
-            seconds = datetime.timedelta(hours=st.tm_hour,minutes=st.tm_min).total_seconds()
-        if self.need_named_events == True and len(namedevent)> 0:
-            self.get_logger().warn(f"Can't find named event: %s (no named events received, yet)" % namedevent)
-            return nan
-        
-        # get to here seconds is either a time since midnight, or a time modifier on the named event time
+            dt = parser.parse(tstr)
+            seconds = timedelta(hours=dt.hour,minutes=dt.minute).total_seconds()
+        else:
+            namedevent=tstr 
+        if len(namedevent) > 0:    
+            if self.need_named_events == True:
+                self.get_logger().warn(f"Can't find named event: '%s' (no named events received, yet)" % namedevent)
+                return nan
+            # get to here seconds is either a time since midnight, or a time modifier on the named event time
+            else:
+                if namedevent in self.named_events:
+                    named_event_seconds = self.named_events[namedevent]
+                else:
+                    self.get_logger().warn(f"Can't find named event: '%s' (not in list)" % namedevent)    
+                    return nan
+            return named_event_seconds + mul * seconds 
+        else:
+            return seconds
+ 
+    def today_str(self):
+        now = datetime.now()
+        self.today = now.strftime("%A")
+        return self.today
 
+    def sun_listener_callback(self,msg):
+        msg = json.loads(msg.data)
+        self.get_logger().info(f"Got sun events: %s" % json.dumps(msg['payload']['events']))
+        self.named_events = msg['payload']['events']
+        if len(self.raw_schedule): 
+            self.process_raw_schedule(self.raw_schedule) # reprocess schedule with latest sun events
+        self.need_named_events = False        
+       
+    def show_schedule(self,schedule):
+        print("--------------------------")
+        for scene in schedule:
+            print(f"%s:" % scene)
+            for field in schedule[scene]:
+                print(f"\t%s:" % field)
+                if isinstance(schedule[scene][field],list):
+                    for list_or_string in schedule[scene][field]:
+                        print(f"\t\t%s" % list_or_string)    
+                else:    
+                    print(f"\t\t%s" % schedule[scene][field])      
+        print("--------------------------")            
+        
+                    
 def main(args=None):
     rclpy.init(args=args)
 
