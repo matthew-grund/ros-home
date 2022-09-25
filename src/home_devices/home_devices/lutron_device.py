9f0b7f80cfa7deb26fd1bdc7f2615f66164dc6c3
Matthew Grund <matthew.grund77@gmail.com>
2022-09-25 13:24:42 -0400

    lutron device is WIP: adding asyncio

--- src/home_devices/home_devices/lutron_device.py
+++ src/home_devices/home_devices/lutron_device.py
@@ -8,16 +8,42 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
+import asyncio
+from pylutron_caseta.smartbridge import Smartbridge
+
+async def run_lutron(address,key,cert,bridge,ros2_node):
+    bridge = Smartbridge.create_tls(
+        "YOUR_BRIDGE_IP", "caseta.key", "caseta.crt", "caseta-bridge.crt"
+    )
 
 class LutronDevice(Node):
-
     def __init__(self):
         super().__init__('lutron_device')
         self.publisher_ = self.create_publisher(String, 'lutron_device_status', 10)
         timer_period = 2.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-
+        self.lutron_keyfile = ""
+        self.lutron_certfile = ""
+        self.lutron_bridgecertfile = ""
+        self.lutron_ipaddress = ""
+        self.do_need_config_msg = True
+        # subscribe to config
+        self.config_sub = self.create_subscription(
+            String,
+            'settings',
+            self.config_callback,
+            10)
+        self.config_sub  # prevent unused variable warning
+        
+        # subscribe to commands
+        self.command_sub = self.create_subscription(
+            String,
+            'commands',
+            self.commannd_callback,
+            10)
+        self.command_sub  # prevent unused variable warning
+        
     def timer_callback(self):
         msg = String()
         msg.data = 'Light Status: %d' % self.i
@@ -25,17 +51,29 @@ class LutronDevice(Node):
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
+    def need_config_msg(self):
+        return self.do_need_config_msg
+    
+    
+async def ros_spin_once(minimal_publisher):
+    rclpy.spin_once(minimal_publisher, timeout_sec=0)
 
-def main(args=None):
+async def run_ros_node(ros2_node):
+    while True:
+        await ros_spin_once(ros2_node)
+        await asyncio.sleep(0)
+
+async def main(args=None):
     rclpy.init(args=args)
-
     device = LutronDevice()
-
-    rclpy.spin(device)
-
+    task_ros2_node = asyncio.create_task(run_ros_node(device))
+    # just the ros task runs, until we get the config, and can open the lutron
+    while device.need_config_msg():
+        await asyncio.sleep(0)
+    task_lutron_bridge = asyncio.create_task(run_lutron(device))
+    await asyncio.gather(task_ros2_node, task_lutron_bridge)
     device.destroy_node()
     rclpy.shutdown()
 
-
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
