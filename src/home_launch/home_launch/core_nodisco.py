a45beab0737a555fd5b0bb5dff7fc1d7bb4576f1
Matthew Grund <matthew.grund77@gmail.com>
2022-09-27 09:07:34 -0400

    added a launcher without discovery, for testing away from home.

---
+++ src/home_launch/home_launch/core_nodisco.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+   # discovery_node = Node(
+   #     package="home_core",
+   #     executable="netdisco",
+   # )
+
+    weather_node = Node(
+        package="home_core",
+        executable="weather"
+    )
+
+    sun_node = Node(
+        package="home_core",
+        executable="sun"
+    )
+
+    event_node = Node(
+        package="home_core",
+        executable="event"
+    )
+ 
+    sms_node = Node(
+        package="home_core",
+        executable="sms"
+    )
+
+    config_node = Node(
+        package="home_core",
+        executable="config"
+    )
+
+    ld.add_action(discovery_node)
+    ld.add_action(weather_node)
+    ld.add_action(sun_node)
+    ld.add_action(event_node)
+    ld.add_action(sms_node)
+    ld.add_action(config_node)
+
+    return ld
