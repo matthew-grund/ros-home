6f1fc843787df2e5e6db1019c69d81bcb2f5c39c
Matthew Grund <matthew.grund77@gmail.com>
2022-10-03 14:07:28 -0400

    added comments to core_launch.py

--- src/home_launch/home_launch/core_launch.py
+++ src/home_launch/home_launch/core_launch.py
@@ -1,39 +1,55 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+#
+#  The "home_core" package has several nodes that every ROS Home would use
+#
 def generate_launch_description():
     ld = LaunchDescription()
 
+    # network discovery of smart home devices
     discovery_node = Node(
         package="home_core",
         executable="netdisco",
     )
 
+    # weather forecast and current conditions can inform 
+    # control decisions, like thermostat setting
     weather_node = Node(
         package="home_core",
         executable="weather"
     )
 
+    # sun position in the sky can inform control decisions,
+    # like lighting activation times, with events published like "Sunrise".
     sun_node = Node(
         package="home_core",
         executable="sun"
     )
 
+    # the core control concept in ROS Home is the event. This node filters data from "track"
+    # nodes, and develops events.
     event_node = Node(
         package="home_core",
         executable="event"
     )
- 
+    
+    # The SMS node sits on the "events" topic, and notifies users by email, with 
+    # SMS email addresses possible.
     sms_node = Node(
         package="home_core",
         executable="sms"
     )
 
+    # The config node monitors a directory for INI files, and publishes contents of those files
+    # to the "settings" topic. This node is flexible, and will publish contents of ANY INI file.
+    #
     config_node = Node(
         package="home_core",
         executable="config"
     )
 
+    # the schedule node reads a scene schedule INI, and publishes 
+    # event-ready time data for the day
     schedule_node = Node(
         package="home_core",
         executable="schedule"
