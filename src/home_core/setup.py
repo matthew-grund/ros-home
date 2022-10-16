99e75e5ebc5e3815de3040719f914ceba2d9b23b
Matthew Grund <matthew.grund77@gmail.com>
2022-10-16 13:35:59 -0400

    core package setup tweaked

--- src/home_core/setup.py
+++ src/home_core/setup.py
@@ -4,7 +4,7 @@ package_name = 'home_core'
 
 setup(
     name=package_name,
-    version='0.0.4',
+    version='0.8.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
