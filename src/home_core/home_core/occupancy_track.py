04b7565e40cb21e0f2a7fd4438e1a2ede6a1258b
Matthew Grund <matthew.grund77@gmail.com>
2022-10-16 13:04:26 -0400

    occupancy tracker takes config now

--- src/home_core/home_core/occupancy_track.py
+++ src/home_core/home_core/occupancy_track.py
@@ -7,6 +7,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String 
 import json
+import math
 
 class OccupancyTracker(Node):
     #  The occupancy_tracker node uses bluetooth discovery methods, and other 
@@ -21,13 +22,64 @@ class OccupancyTracker(Node):
         self.timer_bt = self.create_timer(self.bt_timer_period, self.bt_timer_callback)
         self.n_bt = 0
 
+        self.config_subscription = self.create_subscription(
+            String,
+            'settings',
+            self.config_listener_callback,
+            10)
+        self.config_subscription
+        self.need_config_location = True
+        self.need_config_occupants = True
+
+        self.owntracks_subscription = self.create_subscription(
+            String,
+            'owntracks',
+            self.owntracks_listener_callback,
+            10)
+        self.config_subscription
+        self.need_config_location = True
+        self.need_config_occupants = True
+        self.occupants_are_home = {}
+
+
+    def config_listener_callback(self,msg):
+        msg = json.loads(msg.data)
+        if msg['payload']['type'] == "LOCATION":
+            self.home_latitude = float(msg['payload']['Coordinates']['latitude']) 
+            self.home_longitude = float(msg['payload']['Coordinates']['longitude']) 
+            self.need_config_location = False
+            self.get_logger().info(f"Got config: Location is {self.city}, {self.state}")
+        # todo: add OCCUPANTS config
+
+    def owntracks_listener_callback(self,msg):
+        new_occ = False
+        m = json.loads(msg.data)
+        if m['type'] == 'EVENT' and  m['action'] == 'INFILTRATE' and m['region'].upper() == "HOME":
+            self.occupants_are_home[m['tracked_id']] = True
+            new_occ = True
+        if m['type'] == 'EVENT' and m['action'] == 'EXFILTRATE' and m['region'].upper() == "HOME":
+            self.occupants_are_home[m['tracked_id']] = False
+            new_occ = True
+        if new_occ:
+            self.publish_occupancy()
+            _    
+    def publish_occupancy(self):
+        n_home = 0
+        n_occ = 0
+        occ_list = []
+        for o in self.occupants_are_home:
+            n_occ += 1
+            if self.occupants_are_home[o]:    
+                n_home += 1
+        
+                
     def bt_timer_callback(self):
-        nearby_devices =  bluetooth.discover_devices()
+        # nearby_devices =  bluetooth.discover_devices()
         msg = String()
         msg.data = 'Occupants: %d' % self.n_bt
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.n_bt += 1
+        #self.publisher_.publish(msg)
+        #self.get_logger().info('Publishing: "%s"' % msg.data)
+        #self.n_bt += 1
 
 
 def main(args=None):
