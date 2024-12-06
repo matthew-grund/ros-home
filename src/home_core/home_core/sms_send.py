9d17f2399f30139acb904ace7d4f678ff0ded9e1
Matthew Grund <matthew.grund77@gmail.com>
2024-12-06 11:46:20 -0500

    added startup log mesage

--- src/home_core/home_core/sms_send.py
+++ src/home_core/home_core/sms_send.py
@@ -10,6 +10,8 @@ import smtplib
 import ssl
 import json
 import datetime
+
+
 class SMSMessager(Node):
 
     def __init__(self):
@@ -26,7 +28,9 @@ class SMSMessager(Node):
             self.config_listener_callback,
             10)
         self.config_subscription  # prevent unused variable warning
+        self.get_logger().info(f"Waiting for config message on topic '/home/configuration'")
         self.need_config = True
+
 
     def event_listener_callback(self, msg):
         if not self.need_config:
@@ -37,18 +41,20 @@ class SMSMessager(Node):
             # filter the events
             if subj!="DEVICE":          # device discovery very noisy at the minute
                 self.send(subj,msg)
-    
+
+
     def config_listener_callback(self,msg):
         msg = json.loads(msg.data)
         if msg['payload']['type'] == "SMS":
-            self.port = msg['payload']['Server']['port'] 
+            self.port = msg['payload']['Server']['smtp_port'] 
             self.smtp_server_domain_name = msg['payload']['Server']['smtp_server']
             self.sender_mail = msg['payload']['Sender']['address']
-            self.password = msg['payload']['Sender']['password']
+            self.password = msg['payload']['Sender']['password']  # nxxw clhv vkbt mnyz
             self.dest_mail = msg['payload']['Recipient']['address']
             self.need_config = False
             self.get_logger().info(f"Got config: {self.sender_mail} sending to {self.dest_mail}")
             # TODO - add people config
+
 
     def send(self, subject, content):
         now = datetime.datetime.now()
